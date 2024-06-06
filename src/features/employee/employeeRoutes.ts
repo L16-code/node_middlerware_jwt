1d9c5e6537e1801c3fba69085873c887e36a71a8
L16-code <lavishjain2002@gmail.com>
2024-06-06 12:18:43 +0530

    corrected the routes for delete

--- src/features/employee/employeeRoutes.ts
+++ src/features/employee/employeeRoutes.ts
@@ -3,13 +3,13 @@ import verifyToken from "../../middleware/authMiddleware";
 import { validateRequest } from "../../middleware/validations";
 import { employeeIdSchema, employeeSchema, employeeUpdateSchema } from "./employeeValidations";
 import HandleErrors from "../../middleware/hndleErrors";
-import { createEmployee, getAllEmployees, getEmployee, updateEmployee } from "./employeecontroller";
+import { createEmployee, deleteEmployee, getAllEmployees, getEmployee, updateEmployee } from "./employeecontroller";
 const router = express.Router();
 
 router.get('/employee',verifyToken,HandleErrors(getAllEmployees))
 router.post('/employee',verifyToken,validateRequest(employeeSchema), HandleErrors(createEmployee))
 router.get('/employee/:id',verifyToken,validateRequest(employeeIdSchema,'params'),HandleErrors(getEmployee))
 router.put('/employee/:id',verifyToken,validateRequest(employeeUpdateSchema),validateRequest(employeeIdSchema ,'params') ,HandleErrors(updateEmployee))
-router.delete('/employee/:id',verifyToken,validateRequest(employeeIdSchema,'params'),HandleErrors(updateEmployee))
+router.delete('/employee/:id',verifyToken,validateRequest(employeeIdSchema,'params'),HandleErrors(deleteEmployee))
 
 export default router
